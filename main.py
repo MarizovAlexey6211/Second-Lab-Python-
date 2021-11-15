f1967e4791e136f764e76034d8f2583784b0deb4
MarizovAlexey6211 <mar.karnage@outlook.com>
2021-11-15 08:17:31 +0400

    add class Validator

--- main.py
+++ main.py
@@ -24,3 +24,27 @@ class Record:
     academic_degree: str
     worldview: str
     address: str
+
+    class Validator:
+        """Class for providing validation of Record's instance"""
+
+        def __init__(self, records: Collection[Record]):
+            """Initialization instance of Validator
+
+            Args:
+                records: list of instance of class Records
+            """
+            self.records = records
+            self.processing_log = {
+                'number_valid_records': 0,
+                'number_invalid_records': 0,
+                'invalid_telephone': 0,
+                'invalid_weight': 0,
+                'invalid_inn': 0,
+                'invalid_passport_number': 0,
+                'invalid_occupation': 0,
+                'invalid_work_experience': 0,
+                'invalid_academic_degree': 0,
+                'invalid_worldview': 0,
+                'invalid_address': 0,
+            }
