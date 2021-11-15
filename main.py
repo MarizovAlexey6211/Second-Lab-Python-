a2ca67745aae2ee83ce551d84ebb542d0d37f2c6
MarizovAlexey6211 <mar.karnage@outlook.com>
2021-11-15 13:20:11 +0400

    add list of valid records

--- main.py
+++ main.py
@@ -48,3 +48,38 @@ class Record:
                 'invalid_worldview': 0,
                 'invalid_address': 0,
             }
+
+            def processing(self) -> List[Record]:
+                """Validates record
+
+                Returns:
+                    list of valid records
+                """
+                valid_records = []
+                for record in tqdm(self.records):
+                    self.processing_log['number_invalid_records'] += 1
+
+                    if not self.validation_telephone(record.telephone):
+                        self.processing_log['invalid_telephone'] += 1
+                    elif not self.validation_weight(record.weight):
+                        self.processing_log['invalid_weight'] += 1
+                    elif not self.validation_inn(record.inn):
+                        self.processing_log['invalid_inn'] += 1
+                    elif not self.validation_passport_number(record.passport_number):
+                        self.processing_log['invalid_passport_number'] += 1
+                    elif not self.validation_text_properties(record.occupation):
+                        self.processing_log['invalid_occupation'] += 1
+                    elif not self.validation_work_experience(record.work_experience):
+                        self.processing_log['invalid_work_experience'] += 1
+                    elif not self.validation_text_properties(record.academic_degree):
+                        self.processing_log['invalid_academic_degree'] += 1
+                    elif not self.validation_text_properties(record.worldview):
+                        self.processing_log['invalid_worldview'] += 1
+                    elif not self.validation_address(record.address):
+                        self.processing_log['invalid_address'] += 1
+                    else:
+                        self.processing_log['number_invalid_records'] -= 1
+                        self.processing_log['number_valid_records'] += 1
+                        valid_records.append(record)
+
+                return valid_records
