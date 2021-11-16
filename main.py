060932de3e686c7f0ea3f63afa2bd3d44415de3d
MarizovAlexey6211 <mar.karnage@outlook.com>
2021-11-16 09:23:22 +0400

    end

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from typing import Collection, List
 
 from tqdm import tqdm
 
+
 @dataclass
 class Record:
     """Data class for storing info about scientific staff"""
@@ -25,64 +26,64 @@ class Record:
     worldview: str
     address: str
 
-    class Validator:
-        """Class for providing validation of Record's instance"""
 
-        def __init__(self, records: Collection[Record]):
-            """Initialization instance of Validator
+class Validator:
+    """Class for providing validation of Record's instance"""
+    def __init__(self, records: Collection[Record]):   #коллекция экземпляров класса записей(Record)
+        """Initialization instance of Validator
 
-            Args:
-                records: list of instance of class Records
-            """
-            self.records = records
-            self.processing_log = {
-                'number_valid_records': 0,
-                'number_invalid_records': 0,
-                'invalid_telephone': 0,
-                'invalid_weight': 0,
-                'invalid_inn': 0,
-                'invalid_passport_number': 0,
-                'invalid_occupation': 0,
-                'invalid_work_experience': 0,
-                'invalid_academic_degree': 0,
-                'invalid_worldview': 0,
-                'invalid_address': 0,
-            }
+        Args:
+            records: list of instance of class Records
+        """
+        self.records = records
+        self.processing_log = {
+            'number_valid_records': 0,
+            'number_invalid_records': 0,
+            'invalid_telephone': 0,
+            'invalid_weight': 0,
+            'invalid_inn': 0,
+            'invalid_passport_number': 0,
+            'invalid_occupation': 0,
+            'invalid_work_experience': 0,
+            'invalid_academic_degree': 0,
+            'invalid_worldview': 0,
+            'invalid_address': 0,
+        }
 
-            def processing(self) -> List[Record]:
-                """Validates record
+    def processing(self) -> List[Record]:
+        """Validates record
 
-                Returns:
-                    list of valid records
-                """
-                valid_records = []
-                for record in tqdm(self.records):
-                    self.processing_log['number_invalid_records'] += 1
+        Returns:
+            list of valid records
+        """
+        valid_records = []
+        for record in tqdm(self.records):
+            self.processing_log['number_invalid_records'] += 1
 
-                    if not self.validation_telephone(record.telephone):
-                        self.processing_log['invalid_telephone'] += 1
-                    elif not self.validation_weight(record.weight):
-                        self.processing_log['invalid_weight'] += 1
-                    elif not self.validation_inn(record.inn):
-                        self.processing_log['invalid_inn'] += 1
-                    elif not self.validation_passport_number(record.passport_number):
-                        self.processing_log['invalid_passport_number'] += 1
-                    elif not self.validation_text_properties(record.occupation):
-                        self.processing_log['invalid_occupation'] += 1
-                    elif not self.validation_work_experience(record.work_experience):
-                        self.processing_log['invalid_work_experience'] += 1
-                    elif not self.validation_text_properties(record.academic_degree):
-                        self.processing_log['invalid_academic_degree'] += 1
-                    elif not self.validation_text_properties(record.worldview):
-                        self.processing_log['invalid_worldview'] += 1
-                    elif not self.validation_address(record.address):
-                        self.processing_log['invalid_address'] += 1
-                    else:
-                        self.processing_log['number_invalid_records'] -= 1
-                        self.processing_log['number_valid_records'] += 1
-                        valid_records.append(record)
+            if not self.validation_telephone(record.telephone):
+                self.processing_log['invalid_telephone'] += 1
+            elif not self.validation_weight(record.weight):
+                self.processing_log['invalid_weight'] += 1
+            elif not self.validation_inn(record.inn):
+                self.processing_log['invalid_inn'] += 1
+            elif not self.validation_passport_number(record.passport_number):
+                self.processing_log['invalid_passport_number'] += 1
+            elif not self.validation_text_properties(record.occupation):
+                self.processing_log['invalid_occupation'] += 1
+            elif not self.validation_work_experience(record.work_experience):
+                self.processing_log['invalid_work_experience'] += 1
+            elif not self.validation_text_properties(record.academic_degree):
+                self.processing_log['invalid_academic_degree'] += 1
+            elif not self.validation_text_properties(record.worldview):
+                self.processing_log['invalid_worldview'] += 1
+            elif not self.validation_address(record.address):
+                self.processing_log['invalid_address'] += 1
+            else:
+                self.processing_log['number_invalid_records'] -= 1
+                self.processing_log['number_valid_records'] += 1
+                valid_records.append(record)
 
-                return valid_records
+        return valid_records
 
     @staticmethod
     def validation_telephone(telephone: str) -> bool:
@@ -207,3 +208,22 @@ class Record:
             return True if re.match(pattern, str(address)) else False
         except TypeError:
             return False
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Validation records')#Создаём парсер
+    parser.add_argument('-input', type=str, help='Input file of data')#аргумент, который передается после опции -input, сохранится в переменную type,но прежде выполним отображение с подсказками
+    parser.add_argument('-output', type=str, help='Output file of data')#аргумент, который передается после опции -output, сохранится в переменную type,но прежде выполним отображение с подсказками
+    args = parser.parse_args()#После выполнения, в переменной args содержатся аргументы переданные скрипту
+
+    data = json.load(open(args.input, encoding='windows-1251'))
+    records = [Record(**el) for el in data]
+
+    validator = Validator(records)
+    valid_records = validator.processing()#Работа с функцией которая каждую запись проверяет(валидные данные которые получены после проверки)
+
+    with open(args.output, 'w') as f:##########
+        f.write(pformat([asdict(record) for record in valid_records]))
+
+    pprint(validator.processing_log)
+
